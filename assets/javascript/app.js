56bda1e3f49c14fa902e5b281a65feacf725ba13
unknown <mstevens9335@yahoo.com>
2019-03-11 17:52:28 -0400

    gets from giphy api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,55 +2,55 @@
   var topics = ["dog", "cat", "snake", "fish"];
 
     var topic = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&apikey=eOejIy4du1HXE9xtGZ3ALUhhMUZZlmJl&limit=5";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + "dog" + "&apikey=eOejIy4du1HXE9xtGZ3ALUhhMUZZlmJl&limit=5";
 
    // Creating an AJAX call for the specific movie button being clicked
    $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+    console.log(response);
+    // // Creating a div to hold the movie
+    // var movieDiv = $("<div class='movie'>");
 
-    // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
+    // // Storing the rating data
+    // var rating = response.Rated;
 
-    // Storing the rating data
-    var rating = response.Rated;
+    // // Creating an element to have the rating displayed
+    // var pOne = $("<p>").text("Rating: " + rating);
 
-    // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
+    // // Displaying the rating
+    // movieDiv.append(pOne);
 
-    // Displaying the rating
-    movieDiv.append(pOne);
+    // // Storing the release year
+    // var released = response.Released;
 
-    // Storing the release year
-    var released = response.Released;
+    // // Creating an element to hold the release year
+    // var pTwo = $("<p>").text("Released: " + released);
 
-    // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
+    // // Displaying the release year
+    // movieDiv.append(pTwo);
 
-    // Displaying the release year
-    movieDiv.append(pTwo);
+    // // Storing the plot
+    // var plot = response.Plot;
 
-    // Storing the plot
-    var plot = response.Plot;
+    // // Creating an element to hold the plot
+    // var pThree = $("<p>").text("Plot: " + plot);
 
-    // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
+    // // Appending the plot
+    // movieDiv.append(pThree);
 
-    // Appending the plot
-    movieDiv.append(pThree);
+    // // Retrieving the URL for the image
+    // var imgURL = response.Poster;
 
-    // Retrieving the URL for the image
-    var imgURL = response.Poster;
+    // // Creating an element to hold the image
+    // var image = $("<img>").attr("src", imgURL);
 
-    // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
+    // // Appending the image
+    // movieDiv.append(image);
 
-    // Appending the image
-    movieDiv.append(image);
-
-    // Putting the entire movie above the previous movies
-    $("#buttons-view").prepend(movieDiv);
+    // // Putting the entire movie above the previous movies
+    // $("#buttons-view").prepend(movieDiv);
   });
 
 
